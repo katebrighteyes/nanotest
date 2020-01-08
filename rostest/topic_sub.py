e8498fc3eb373f795a39b2a31851c424f29e7592
Kate <49902595+katebrighteyes@users.noreply.github.com>
2020-01-08 17:41:14 +0900

    Update topic_sub.py

--- rostest/topic_sub.py
+++ rostest/topic_sub.py
@@ -3,14 +3,14 @@
 import rospy
 from std_msgs.msg import String
 
-def callback(msg):  # 콜백 함수 정의
+def callback(msg):  # callback function
     print msg.data
 
-rospy.init_node(‘topic_sub’) 
-# 노드 이름을 topic_sub로 정의한다.
+rospy.init_node("topic_sub") 
+# define node name "topic_sub"
 
-sub = rospy.Subscriber(‘test_string’, String, callback)
-# test_string 토픽 처리를 위한 callback함수 등록
+sub = rospy.Subscriber("test_string", String, callback)
+# Register callback function for test_string topic
 
 rospy.spin() 
-# ROS에게 제어를 넘긴다. 노드가 종료할 경우 리턴된다.
+
