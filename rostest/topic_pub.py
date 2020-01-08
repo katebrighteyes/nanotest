2b0c69b4b633fe7d3f873092af3df594f3aa5668
Kate <49902595+katebrighteyes@users.noreply.github.com>
2020-01-08 17:38:19 +0900

    Update topic_pub.py

--- rostest/topic_pub.py
+++ rostest/topic_pub.py
@@ -3,14 +3,14 @@
 import rospy
 from std_msgs.msg import String
 
-rospy.init_node(‘topic_pub’)
-# 노드 이름을 topic_pub로 정의한다.
+rospy.init_node("topic_pub")
+# define node name as 'topic_pub'
 
-pub = rospy.Publisher(‘test_string’, String) 
-# 토픽에 test_string 이라는 이름을 부여한다.
+pub = rospy.Publisher("test_string", String) 
+# give name test_string to the topic
 
-rate = rospy.Rate(2) #발행 속도는 초당 2번
+rate = rospy.Rate(2) 
 
 while not rospy.is_shutdown():
-    pub.publish(‘AAA-BBB')
+    pub.publish("Hello ROS")
     rate.sleep()
